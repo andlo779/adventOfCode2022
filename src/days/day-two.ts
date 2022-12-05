efd3bf7fe186c18228a55a124c7f143f016cacf5
Andreas Lord <andreas.lord@climatepartner.com>
2022-12-05 11:42:33 +0100

    Added solution for day two part two

--- src/days/day-two.ts
+++ src/days/day-two.ts
@@ -5,17 +5,32 @@ export class DayTwo {
   solveChallange(): Answer {
     const fileContent = FileReader.readFile('day-two.txt');
     const list = fileContent.split('\n');
+
     let firstResult = 0;
     list.forEach((g) => {
-      const game = this.parsGameRound(g);
+      const game = this.parsGameRound(g, false);
       firstResult += this.calculateScore(game.first, game.second);
     });
-    return { first: String(firstResult), second: 'NA' };
+
+    let secondResult = 0;
+    list.forEach((g) => {
+      const game = this.parsGameRound(g, true);
+      secondResult += this.calculateScore(game.first, game.second);
+    });
+
+    return { first: String(firstResult), second: String(secondResult) };
   }
 
-  private parsGameRound(input: string): { first: string; second: string } {
+  private parsGameRound(
+    input: string,
+    isStrategy: boolean
+  ): { first: string; second: string } {
     const split = input.split(' ');
-    return { first: split[0], second: split[1] };
+    if (isStrategy) {
+      return this.translateFromStrategyToGamePlay(split[0], split[1]);
+    } else {
+      return { first: split[0], second: split[1] };
+    }
   }
 
   private calculateScore(first: string, second: string): number {
@@ -94,6 +109,76 @@ export class DayTwo {
         break;
       case 'Z':
         result += 3;
+        break;
+    }
+    return result;
+  }
+
+  private translateFromStrategyToGamePlay(
+    first: string,
+    second: string
+  ): {
+    first: string;
+    second: string;
+  } {
+    let result = { first: first, second: 'NA ' };
+    switch (first) {
+      case 'A':
+        result.second = this.handleRockStrateg(second);
+        break;
+      case 'B':
+        result.second = this.handlePaperStrateg(second);
+        break;
+      case 'C':
+        result.second = this.handleSiccorsStrateg(second);
+        break;
+    }
+    return result;
+  }
+
+  private handleRockStrateg(input: string): string {
+    let result = 'NA';
+    switch (input) {
+      case 'X':
+        result = 'Z';
+        break;
+      case 'Y':
+        result = 'X';
+        break;
+      case 'Z':
+        result = 'Y';
+        break;
+    }
+    return result;
+  }
+
+  private handlePaperStrateg(input: string): string {
+    let result = 'NA';
+    switch (input) {
+      case 'X':
+        result = 'X';
+        break;
+      case 'Y':
+        result = 'Y';
+        break;
+      case 'Z':
+        result = 'Z';
+        break;
+    }
+    return result;
+  }
+
+  private handleSiccorsStrateg(input: string): string {
+    let result = 'NA';
+    switch (input) {
+      case 'X':
+        result = 'Y';
+        break;
+      case 'Y':
+        result = 'Z';
+        break;
+      case 'Z':
+        result = 'X';
         break;
     }
     return result;
